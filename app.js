afdb81364267864787eb6f56e481bac91fe5a42c
isurudotxyz <agisuru@icloud.com>
2023-08-21 12:08:18 +0200

    expand and close notes

---
+++ app.js
@@ -0,0 +1,40 @@
+class App {
+  constructor() {
+    console.log("app works");
+    this.$form = document.querySelector("#form");
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$formButtons = document.querySelector("#form-buttons");
+
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    document.body.addEventListener("click", (e) => {
+      this.handleFormClick(e);
+    });
+  }
+
+  handleFormClick(e) {
+    const isFormClicked = this.$form.contains(e.target);
+
+    if (isFormClicked) {
+      // open
+      this.openForm();
+    } else {
+      //close
+    }
+  }
+
+  openForm() {
+    this.$form.classList.add("form-open");
+    this.$noteTitle.style.display = "block";
+    this.$formButtons.style.display = "block";
+  }
+
+  closeForm() {
+    this.$form.classList.remove("form-open");
+    this.$noteTitle.style.display = "none";
+    this.$formButtons.style.display = "none";
+  }
+}
+new App();

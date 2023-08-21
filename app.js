795bbd6800b8aa8d4eccc2a9b060f32f9b6b2ca0
isurudotxyz <agisuru@icloud.com>
2023-08-21 14:22:30 +0200

    display note

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 class App {
   constructor() {
     this.notes = [];
-
+    this.$placeHolder = document.querySelector("#placeholder");
+    this.$notes = document.querySelector("#notes");
     this.$form = document.querySelector("#form");
     this.$noteTitle = document.querySelector("#note-title");
     this.$noteText = document.querySelector("#note-text");
     this.$formButtons = document.querySelector("#form-buttons");
+    this.$formCloseButtons = document.querySelector("#form-close-button");
 
     this.addEventListeners();
   }
@@ -28,6 +30,7 @@ class App {
         });
       }
     });
+    // this.
   }
 
   handleFormClick(e) {
@@ -52,6 +55,8 @@ class App {
     this.$form.classList.remove("form-open");
     this.$noteTitle.style.display = "none";
     this.$formButtons.style.display = "none";
+    this.$noteText.value = " ";
+    this.$noteTitle.value = " ";
   }
 
   addNote(note) {
@@ -62,7 +67,31 @@ class App {
       id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
     };
     this.notes = [...this.notes, newNote];
-    console.log(this.notes);
+    this.displayNotes();
+    this.closeForm();
+  }
+  displayNotes() {
+    // check if notes exist
+    const hasNotes = this.notes.length > 0;
+    this.$placeHolder.style.display = hasNotes ? "none" : "flex";
+    // iterate
+
+    this.$notes.innerHTML = this.notes
+      .map(
+        (note) => `
+    <div style="background: ${note.color};" class="note">
+      <div class="${note.title && "note-title"}>${note.title}</div>
+      <div class="note-text">${note.text}</div>
+      <div class="toolbar-container">
+        <div class="toolbar">
+          <img class="toolbar-color" src="https://icon.now.sh/palette">
+          <img class="toolbar-delete" src="https://icon.now.sh/delete">
+        </div>
+      </div>
+    </div>
+   `
+      )
+      .join("");
   }
 }
 new App();
